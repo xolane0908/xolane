aa572b8eb15655ea22cd1bb060d7f99e21d1fa8f
xolane0908 <xolanemathenjwa@icloud.com>
2025-05-08 12:26:46 +0200

    Add files via upload

---
+++ conversions.py
@@ -0,0 +1,15 @@
+float_num = 99.23
+int_num = int(float_num)
+print(int_num)
+
+int_num = 23
+float_num = float(int_num)
+print(float_num)
+
+int_num2 = 150
+string = str(int_num2)
+print(str)
+
+string_value = ("100")
+int_num3 = int(string_value)
+print(int_num3)
---
+++ details.py
@@ -0,0 +1,11 @@
+user_name = ("His name Patrick.")
+
+user_age = ( "He is 22 yaers old.")
+
+house_address = ("He stays at house number 43")
+
+street_name = (" on Zambezi River road.")
+
+print(user_name + user_age + house_address + street_name)
+
+
